62a29795b54a94ead17372f80f2dd171e23e352f
tshandan <496509048@qq.com>
2015-11-18 20:10:53 +0800

    Initial Commit

---
+++ class/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  class
+//
+//  Created by shandan on 15/11/18.
+//  Copyright (c) 2015年 shandan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
